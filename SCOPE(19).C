87a970e979e8a65ae8b8c5e30b18d8d467a9bad0
CodeWithSmile <166915584+CodeWithSmile@users.noreply.github.com>
2024-04-13 17:04:54 +0530

    Scope In C Programmimg

---
+++ SCOPE(19).C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+//global variable definition
+	int z;
+void main ()
+{
+    //local variable definition and initialization
+   // int x,y,z;
+	//local variable definition and initialization
+	int x,y;
+
+    //actual initialization
+    x = 20;
+    y = 30;
+    z = x + y;
+
+    printf ("value of x = %d, y = %d and z = %d\n", x, y, z);
+
+    getch();
+}
