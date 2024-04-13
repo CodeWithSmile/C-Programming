e9986ef89e1888fc2ce81f54b958a930204013db
CodeWithSmile <166915584+CodeWithSmile@users.noreply.github.com>
2024-04-13 16:43:24 +0530

    Datatype in C programming
    All Necessary datatype in C programming with their example

---
+++ DATATYPE(2).C
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+char b,ch[]="";
+float c;
+clrscr();
+printf("enter character\n");
+scanf("%c",&b);
+printf("\nchar value is=%c",b);
+printf("\nenter a interger value\n");
+scanf("%d",&a);
+printf("\n int value is= %d",a);
+printf("\nenter float value \n");
+scanf("%f",&c);
+printf("\nfloat value=%f",c);
+printf("\nenter a string");
+scanf("%s",&ch);
+printf("string =%s",ch);
+
+
+getch();
+}
+
+
+
